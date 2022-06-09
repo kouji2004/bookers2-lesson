b78e3468062d04a30ee28e478f2e524542afefd0
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-09 08:21:48 +0000

    いいね機能を実装

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users
+   get "home/about"=>"homes#about", as:"about"
+   root to:"homes#top"
+
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]do
+    resource :favorite,only: [:create,:destroy]
+  end
+  resources :users, only: [:show, :edit, :index, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
