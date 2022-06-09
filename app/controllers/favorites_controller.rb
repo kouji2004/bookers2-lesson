25c9c99f7e65b21c007cd2ae50ab8bdb872b0f26
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-09 08:50:56 +0000

    投稿コメント

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,15 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-     favorite = current_user.favorites.new(book_id: @book.id)
-     favorite.save
+     favorites = current_user.favorites.new(book_id: @book.id)
+     favorites.save
      redirect_to book_path(@book)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorite.find_by(book_id: @book.id)
-    favorite.destroy
+    favorites = current_user.favorites.find_by(book_id: @book.id)
+    favorites.destroy
     redirect_to book_path(@book)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
    root to:"homes#top"
 
   resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]do
+   resources :book_comments, only: [:create,:destroy]
     resource :favorite,only: [:create,:destroy]
   end
   resources :users, only: [:show, :edit, :index, :update]
