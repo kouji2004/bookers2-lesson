c90a8773eaf17aaf3457a44065d4defee90daa6b
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-10 14:36:01 +0000

    フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,31 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  #フォローの関係(２行目は一覧画面で使う)自分がフォローする
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  #フォロワーの関係(２行目は一覧画面で使う)自分がフォローされる
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  # フォローしたときの処理
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  # フォローを外すときの処理
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
+
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+    #following_relationships.exists?(following_id: user.id)
+  end
+
   has_one_attached :profile_image
 
   validates :name, uniqueness: true, length: {minimum: 2, maximum: 20 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_082009) do
+ActiveRecord::Schema.define(version: 2022_06_10_072154) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2022_06_09_082009) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "users", force: :cascade do |t|
